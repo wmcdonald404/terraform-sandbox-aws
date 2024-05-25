ddf2f5efbb8a1dffece4c62a021b6382aba1a142
wmcdonald404 <wmcdonald@gmail.com>
2024-05-25 13:24:15 +0100

    - Update the README.md with steps - Add .gitignore to exclude .terraform and HCL lock files - Add vars from source document

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ Simple Terraform playground to create a VPC, associated resources and EC2 instan
 
 ## Usage
 1. Clone the repository
-2. 
+2. Initialise the Terraform configuration
+3. Validate the Terraform configuration
+4. Review the Terraform plan
+5. Apply the Terraform configuration
+
 
 ## References
 - [How to Build AWS VPC using Terraform – Step by Step](https://spacelift.io/blog/terraform-aws-vpc)
--- vars.tf
+++ vars.tf
@@ -1,5 +1,17 @@
 variable "azs" {
   type        = list(string)
   description = "Availability Zones"
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  default     = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
